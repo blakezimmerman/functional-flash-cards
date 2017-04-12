7de28c37eb9703db201e005ac797e9c20efd9680
Blake Zimmerman <blake.zimmerman@icloud.com>
2017-04-12 01:23:51 -0400

    Add Create and Study modes

--- src/Main.elm
+++ src/Main.elm
@@ -9,19 +9,25 @@ main =
 
 -- MODEL
 
+type Mode
+  = Create
+  | Study
+
 type alias Card =
   { frontCard : String
   , backCard : String
   }
 
 type alias Model =
-  { currentCard : Card
+  { mode : Mode
+  , currentCard : Card
   , cardList: List Card
   }
 
 model : Model
 model =
-  { currentCard = Card "" ""
+  { mode = Create
+  , currentCard = Card "" ""
   , cardList = []
   }
 
@@ -29,13 +35,21 @@ model =
 -- UPDATE
 
 type Msg
-  = FrontCard String
+  = CreateMode
+  | StudyMode
+  | FrontCard String
   | BackCard String
   | AddCard
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
+    CreateMode ->
+      { model | mode = Create }
+
+    StudyMode ->
+      { model | mode = Study }
+
     FrontCard front ->
       { model | currentCard = { frontCard = front, backCard = model.currentCard.backCard } }
 
@@ -45,21 +59,32 @@ update msg model =
     AddCard ->
       { model | cardList = model.currentCard :: model.cardList }
 
+
 -- VIEW
 
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ placeholder "Front of Card", onInput FrontCard ] []
-    , input [ placeholder "Back of Card", onInput BackCard ] []
-    , button [ onClick AddCard ] [ text "Add Card" ]
+    [ div []
+        [ button [ onClick CreateMode ] [ text "Create Mode" ]
+        , button [ onClick StudyMode ] [ text "Study Mode" ]
+        ]
     , div []
-      [ renderCardList model.cardList
-      ]
+        [ if model.mode == Create
+          then addCardForm model
+          else renderCardList model.cardList
+        ]
     ]
 
 
---viewCardList : Maybe List Card -> Html Msg
+addCardForm : Model -> Html Msg
+addCardForm model =
+  div []
+    [ input [ placeholder "Front of Card", onInput FrontCard ] []
+    , input [ placeholder "Back of Card", onInput BackCard ] []
+    , button [ onClick AddCard ] [ text "Add Card" ]
+    ]
+
 renderCardList : List Card -> Html Msg
 renderCardList cardList =
   ul []
